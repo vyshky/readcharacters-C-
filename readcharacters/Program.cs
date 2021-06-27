bda52bb425f5f0d7d01b7236e31ab66f20ec214c
Alexander Dedykhin <vyshky2@gmail.com>
2021-06-27 15:18:49 +0400

    First programm

---
+++ readcharacters/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace readcharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte countcharacter = 0;
+            byte countspace = 0;
+            ConsoleKeyInfo i = Console.ReadKey();
+
+
+            while (i.KeyChar != '.')
+            {
+                switch (i.KeyChar)
+                {
+                    case '\r':
+                        Console.WriteLine();
+                        break;
+                    case '\b':
+                        if (countcharacter != 0)
+                        {
+                            Console.Write(" \b");
+                            countcharacter--;
+                        }
+
+                        break;
+                    case ' ':
+                        countspace++;
+                        break;
+                    default:
+                        if (i.KeyChar != '.') countcharacter++;
+                        break;
+                }
+                i = Console.ReadKey();
+            }
+
+            Console.WriteLine($"\nВы ввели {countcharacter} символов");
+            Console.WriteLine($"Вы ввели {countspace} пробелов");
+        }
+    }
+}
